bbbd42e8f87ec1c66183081e3ad10181f66fea99
Alexis Rozas-Parra <ws_virus_wse@hotmail.com>
2015-04-01 13:20:07 +0200

    Some updating to the Rating. Still work in progress.

--- CRUD7/bookStore-front/js/controllers/ratingController.js
+++ CRUD7/bookStore-front/js/controllers/ratingController.js
@@ -1,16 +1,30 @@
+//Somewhere in the controller THAT DISPLAYS FROM THE DATABASE
+// we need to add the possibility to count 0.1 per step on the star.
+
+
+//--We need to duplicate the entire code and make modifications so that it can display the average rating
+//from the database. It will only need the display function and not the click function.--
+//Duplication made, but still needs some more modifications.
+
+
+//!!!!!Also remember that the $window and saverRatingToServer function is later changed to actually
+// save total rating as well as add votecounter to the database.
+// Maybe use the $watch to react and save the rating to the database?
 angular.module('Rater', [])
   .controller('rateCtrl', function($scope, $window, $http) {
     $scope.rating = 5;
     $scope.saveRatingToServer = function(rating) {
       $window.alert('Rating selected - ' + rating);
     };
+    $scope.$watch("rating", function(newVal, oldVal) {
+      if (newVal === undefined) { return; }
+      //The following 2 lines are pretty much the same thing.
+      //It shows in the console log what the rating has been changed to.
+      console.log("rating changed to: ", newVal);
+      console.log("rating changed to: ", $scope.rating);
 
-    $http
-        .get("data.bookData.json")
-        .success(function(data){
-           console.log("Got dummydata", data)
-           $scope.bookData = data;
-  });
+    })
+  })
   .directive('dirRating', function () {
     return {
       restrict: 'A',
@@ -38,6 +52,7 @@ angular.module('Rater', [])
           if (scope.readonly && scope.readonly === 'true') {
             return;
           }
+
           scope.ratingValue = index + 1;
           scope.onRatingSelected({rating: index + 1});
         };
@@ -50,3 +65,33 @@ angular.module('Rater', [])
       }
     }
   });
+
+
+
+
+//Add code for calculating rating from the rating system here
+
+
+
+
+
+
+
+// angular.module('ui.bootstrap.demo').controller('RatingDemoCtrl', function ($scope) {
+//   $scope.rate = 7;
+//   $scope.max = 10;
+//   $scope.isReadonly = false;
+
+//   $scope.hoveringOver = function(value) {
+//     $scope.overStar = value;
+//     $scope.percent = 100 * (value / $scope.max);
+//   };
+
+//   $scope.ratingStates = [
+//     {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+//     {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+//     {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+//     {stateOn: 'glyphicon-heart'},
+//     {stateOff: 'glyphicon-off'}
+//   ];
+// });
---
+++ CRUD7/bookStore-front/js/controllers/showDataRating.js
@@ -0,0 +1,42 @@
+//Change this code to display the average rating from database.
+//Equation is totalRating/voteCount = average
+//Needs more modifications to this code to make it fully functional.
+  .controller('showRateCtrl', ['$scope', function ($scope) {
+    $scope.ratings = [{
+        current: 0,
+        max: 5
+    }  //Calculate and display average rating. Dunno where to insert this, yet. ::: Hugo said this was the spot.
+  $scope.average = function(RatingData){ 
+    var totalRating = 0; for(var i = 0; i < voteCount; i++;){
+    totalRating += parseInt(totalRating[i]); 
+
+    var avg = totalRating/voteCount;
+
+    $scope.ratings[0].current = avg;
+
+    return avg; 
+};
+    }];
+  .directive('dirShowRating', function () {
+    return {
+      restrict: 'A',
+      template: '<ul class="rating2">' +
+                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '=',
+        max: '=',
+      },
+      link: function (scope, elem, attrs) {
+            scope.stars = [];
+            for (var i = 0; i < scope.max; i++) {
+                scope.stars.push({
+                    filled: i < scope.ratingValue
+                });
+            }
+        }
+    }
+});
+
